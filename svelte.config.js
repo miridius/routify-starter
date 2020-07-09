98f803bc5455d5291947e1439168680609c8bcb1
David Rolle <miridius@miridi.us>
2020-07-09 13:13:10 +0200

    Add postcss autoprefixer

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
 import autoPreprocess from 'svelte-preprocess'
 import postcssImport from 'postcss-import'
 
+const production = !process.env.ROLLUP_WATCH
+
 export default {
   preprocess: [
     autoPreprocess({
-      postcss: { plugins: [postcssImport()] },
+      sourceMap: !production,
+      postcss: { plugins: [postcssImport(), require('autoprefixer')()] },
       defaults: { style: 'postcss' },
     }),
   ],
